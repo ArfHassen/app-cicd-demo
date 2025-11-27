64c7d3fc0c98f8746d2bca51bb8761c63e4fbb90
harfaoui <hassen.arfaoui@softeam.fr>
2025-11-27 11:35:13 +0100

    add first endpoint

---
+++ src/main/java/org/acme/demo/rest/MainResource.java
@@ -0,0 +1,16 @@
+package org.acme.demo.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainResource {
+
+    @GetMapping("/sayhello")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello !!");
+    }
+}
